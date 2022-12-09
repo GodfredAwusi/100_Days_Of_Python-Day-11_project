897254f5f8ac499e0de8825196353adcf7ecf5b9
GodfredAwusi <4231220071@live.gctu.edu.gh>
2022-12-09 12:24:03 +0000

    First commit

---
+++ Day-11_project(blackjack_program).py
@@ -0,0 +1,72 @@
+import random
+import os
+from art import logo
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+def deal_card():
+    card = random.choice(cards)
+    return card
+
+def calculate_score(cards):
+    if len(cards) == 2 and sum(cards) == 21:
+        return 0
+    for card in cards:
+        if card == 11 and sum(cards) > 21:
+            card = 1
+    return sum(cards)
+
+def compare(player_score, computer_score):
+    if player_score == computer_score:
+        return "It's a draw!"
+    elif computer_score == 0:
+        return "You lose!, computer has blackjack"
+    elif player_score == 0:
+        return "You have blackjack, you win!"
+    elif player_score > 21:
+        return "You went over, you lose!"
+    elif computer_score > 21:
+        return "Computer went over, you win!"
+    elif player_score > computer_score:
+        return "You win!"
+    else:
+        return "You lose!"
+
+def play_game():
+    player_cards = []
+    computer_cards = []
+    game_over = False
+    print(logo)
+    player_cards.extend([deal_card(), deal_card()])
+    computer_cards.extend([deal_card(), deal_card()])
+
+    while not game_over:
+        player_score = calculate_score(player_cards)
+        computer_score = calculate_score(computer_cards)
+
+        computer_first_card = computer_cards[0]
+
+        print(f"Your cards: {player_cards}, current score: {player_score}")
+        print(f"Computer's first card: {computer_first_card}")
+
+        if player_score == 0 or computer_score == 0 or player_score > 21:
+            game_over = True
+        else:
+            get_another_card = input("Type 'y' to get another card or type 'n' to pass: ").lower()
+            if get_another_card == "y":
+                player_cards.append(deal_card())
+            else:
+                game_over = True
+    while computer_score < 17 and computer_score != 0:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+    print(f"   Your final hand: {player_cards}, final score: {player_score}")
+    print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
+    print(compare(player_score=player_score, computer_score=computer_score))
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+    cls()
+    play_game()  
---
+++ README.md
@@ -0,0 +1,18 @@
+# Blackjack Game
+### The deck is unlimited in size. 
+### There are no jokers. 
+### The Jack/Queen/King all count as 10.
+### The the Ace can count as 11 or 1.
+### The cards in the list have equal probability of being drawn.
+### Cards are not removed from the deck as they are drawn.
+### The computer is the dealer.
+```
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+```
